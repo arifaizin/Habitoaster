843fd551df642bd5f86d59c42f41f9c6f225a4cc
arifaizin <faizin.arief@gmail.com>
2017-10-16 17:22:30 +0700

    Initial commit

---
+++ app/src/main/java/org/sandec/habitoaster/RewardFragment.java
@@ -0,0 +1,29 @@
+package org.sandec.habitoaster;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RewardFragment extends Fragment {
+
+
+    public RewardFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_reward, container, false);
+    }
+
+}
