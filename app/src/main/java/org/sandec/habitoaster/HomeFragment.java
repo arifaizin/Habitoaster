8360585a0eb5caabc174d5c96f3a5021214aca9d
DESKTOP-17QH47K\Febriyan A. Fatma <febriyanadifatma13021996@gmail.com>
2017-10-18 15:43:18 +0700

    penambahan swiperefresh

--- app/src/main/java/org/sandec/habitoaster/HomeFragment.java
+++ app/src/main/java/org/sandec/habitoaster/HomeFragment.java
@@ -1,31 +1,59 @@
 package org.sandec.habitoaster;
 
 import android.os.Bundle;
-import android.support.design.widget.CollapsingToolbarLayout;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class HomeFragment extends Fragment {
+
     public HomeFragment(){
 
     }
+    SwipeRefreshLayout swLayout;
+    LinearLayout llayout;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
 
-        CollapsingToolbarLayout collapsingToolbar =
-                (CollapsingToolbarLayout) v.findViewById(R.id.collapsing_toolbar);
-// Set title of Detail page
-        collapsingToolbar.setTitle(getString(R.string.item_title));
+        swLayout = (SwipeRefreshLayout) v.findViewById(R.id.swlayout);
+
+        llayout = (LinearLayout) v.findViewById(R.id.detail_content);
+
+        swLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                // Handler untuk menjalankan jeda selama 5 detik
+                new Handler().postDelayed(new Runnable() {
+                    @Override public void run() {
+
+                        // Berhenti berputar/refreshing
+                        swLayout.setRefreshing(false);
+
+                        // fungsi-fungsi lain yang dijalankan saat refresh berhenti
+                        return;
+                    }
+                }, 5000);
+            }
+        });
+
+//        CollapsingToolbarLayout collapsingToolbar =
+//                (CollapsingToolbarLayout) v.findViewById(R.id.collapsing_toolbar);
+//// Set title of Detail page
+//        collapsingToolbar.setTitle(getString(R.string.item_title));
         RecyclerView targetRecyclerView = (RecyclerView)v.findViewById(R.id.daftar_menu);
         GridLayoutManager gridLayout = new GridLayoutManager(getActivity(), 1);
         targetRecyclerView.setLayoutManager(gridLayout);
